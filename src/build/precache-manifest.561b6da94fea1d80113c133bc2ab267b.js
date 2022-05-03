df76cf3d8197b2aa738723fb4f2c30f9f6a05e35
Mengzhe Wu <978374095@qq.com>
2022-05-02 23:14:06 -0400

    First Commit

---
+++ src/build/precache-manifest.561b6da94fea1d80113c133bc2ab267b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e12b2fdf87a866212bf9",
+    "url": "/static/css/main.f9999805.chunk.css"
+  },
+  {
+    "revision": "e12b2fdf87a866212bf9",
+    "url": "/static/js/main.c363dd6b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "de1de87e8c2ed82e07bd",
+    "url": "/static/js/2.14e6b8d3.chunk.js"
+  },
+  {
+    "revision": "19c4e4f98768da33bb578c6ac0181c17",
+    "url": "/index.html"
+  }
+];
